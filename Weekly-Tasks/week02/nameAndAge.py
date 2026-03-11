69ca3667fd4fa95fa7b359f04836730e55324216
Martynas-Ramonas <G00486620@atu.ie>
2026-03-11 16:23:05 +0000

    Initial name and age set up

---
+++ Weekly-Tasks/week02/nameAndAge.py
@@ -0,0 +1,10 @@
+# Weekly Tasks 2 Extra
+# Program to read you name and age and output a message with your name and age
+# Author: Martynas Ramonas
+
+# Get the user's name and age
+name = input('Please enter your name: ')
+age = input ('Please enter your age: ')
+
+# Output a message with the user's name and age
+print(f'Hello {name}, your age is {age}.')
