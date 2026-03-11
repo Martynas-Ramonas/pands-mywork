44005852160d19e6f5a10194690fec2aa989a217
Martynas-Ramonas <G00486620@atu.ie>
2026-03-11 18:27:35 +0000

    Finished program to pick a random fruit from a touple instead of a list

---
+++ Weekly-Tasks/week03/lab3.1.5.2_randomFruit2.py
@@ -0,0 +1,16 @@
+# Weekly Task 3
+# Program that prints out a random fruit
+# Author: Martynas Ramonas
+
+# import the random module to use its functions for generating random numbers.
+import random
+
+# This uses a touple() instead of a list[]. A tuple is similar to a list but it can't be changed later
+fruits = ('Apple', 'Banana', 'Orange', 'Grape', 'Kiwi', 'Mango', 'Pineapple')
+
+# We want a random number between 0 and length-1
+index = random.randint(0, len(fruits) - 1)
+
+# ???
+fruit = fruits[index]
+print(f'Here is a random fruit: {fruit}')
