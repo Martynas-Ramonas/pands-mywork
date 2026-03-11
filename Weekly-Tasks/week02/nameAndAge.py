a02a442266171c38eb15452a6546f212fb56e474
Martynas-Ramonas <G00486620@atu.ie>
2026-03-11 16:29:54 +0000

    Removed the # from code so it all runs at once

--- Weekly-Tasks/week02/nameAndAge.py
+++ Weekly-Tasks/week02/nameAndAge.py
@@ -3,12 +3,15 @@
 # Author: Martynas Ramonas
 
 # Get the user's name and age
-#name = input('Please enter your name: ') # Commented out
-#age = input ('Please enter your age: ') # Commented out
+name = input('Please enter your name: ')
+age = input ('Please enter your age: ') 
 
 # Output a message with the user's name and age
 # The \t is a tab character
-#print(f'Hello {name},\t your age is {age}.') # Commented out
+print(f'Hello {name},\t your age is {age}.') 
 
 #Outputs the result of 21 - 4
-print(21 - 4)
+print(21 - 4) 
+
+#Checks if 2 is equal to 3 and outputs the result
+print(2==3)
