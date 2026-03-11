79afab4a7248bef383d3ad637efa9354bff74e68
Martynas-Ramonas <G00486620@atu.ie>
2026-03-11 18:47:25 +0000

    Finished round numbers program

---
+++ Weekly-Tasks/week03/lab3.2.1_round.py
@@ -0,0 +1,14 @@
+# Weekly Task 3
+# Program that takes in a float and the output is rounded to the nearest whole number
+# 4.5 rounds to 4, 5.5 rounds to 6, so do not use it accuracy is essential
+# Author: Martynas Ramonas
+
+# Get the number from the user
+number_to_round = float(input('Enter a float number: '))
+
+# Use the round() function to round a number to the nearest whole number.
+rounded_number = round(number_to_round)
+
+# Print the rounded number
+print(f'{number_to_round} rounded to the nearest whole number is {rounded_number}')
+
