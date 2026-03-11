260d1f28fb4043153f1653748a11f0e77c8cbfd9
Martynas-Ramonas <G00486620@atu.ie>
2026-03-11 18:55:16 +0000

    Finished the absolute program

---
+++ Weekly-Tasks/week03/lab3.2.2_absolute.py
@@ -0,0 +1,14 @@
+# Weekly Task 3
+# Program that takes in a number give it absolute value
+# Author: Martynas Ramonas
+
+# Get the number from the user
+number = float(input('Enter a number: '))
+
+# Use the abs() function to get the absolute value of a number.
+#The abs() function returns the absolute value of a number, which is the distance of the number from zero on the number line. 
+# For example, the absolute value of -5 is 5, and the absolute value of 5 is also 5.
+absolute_value = abs(number)
+
+# Print the absolute value
+print(f'The absolute value of {number} is {absolute_value}')
