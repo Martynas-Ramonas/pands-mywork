b02e151a83ddf5567c7b2b03489f4c04cea5996b
Martynas-Ramonas <G00486620@atu.ie>
2026-03-11 14:42:10 +0000

    Added 'Nice to meet you' and a f-string variant

--- Weekly-Tasks/week02/hello2.py
+++ Weekly-Tasks/week02/hello2.py
@@ -3,4 +3,11 @@
 # Author: Martynas Ramonas
 
 name = input("Enter your name:")
-print("Hello " + name)
+
+# Needs a space after "Hello" to separate it from the name otherwise it's joined together
+# The '\n' is a newline character that moves the next part of the message to a new line like 'Return'/'Enter' key on the keyboard
+print("Hello " + name + '\nNice to meet you')
+
+# Using an f-string(formated string) for the same output as above
+#print(f'Hello {name}\nNice to meet you')  #Commented out so theres no double output
+
