54d48a820ce8ba34be64c3ad6671274b8f264416
Martynas-Ramonas <G00486620@atu.ie>
2026-03-11 14:53:04 +0000

    Add one number to wahtever number is entered

---
+++ Weekly-Tasks/week02/addOne.py
@@ -0,0 +1,12 @@
+#Weekly Tasks 2
+# Adding 1 to a number
+# Author: Martynas Ramonas
+
+# Converts the input to an integer
+number = int(input("Enter a number:"))  
+
+# Adds 1 to the number
+newNumber = number + 1  
+
+# Using an f-string to format the output, showing the original number and the new number after adding 1
+print(f'{number} plus one is {newNumber}') 
