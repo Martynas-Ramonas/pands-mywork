7f9a8646aaee53f3d25045837910e2e2cbfa5f83
Martynas-Ramonas <G00486620@atu.ie>
2026-03-11 20:06:24 +0000

    Finished modifying hte program to accept any length of numbers and mask all but last 4

--- Weekly-Tasks/week03/task3_accounts.py
+++ Weekly-Tasks/week03/task3_accounts.py
@@ -4,13 +4,13 @@
 # Author: Martynas Ramonas
 
 # Get the account number from the user
-account_number = input('Enter a 10 digit account number: ')
+account_number = input('Enter any number of digits for the account number: ')
 
 # Use string slicing to get the last 4 digits of the account number good tutorial on W3Schools https://www.w3schools.com/python/python_strings_slicing.asp
-last_four = account_number[6:]
-
-# Create the masked account number by concatenating Xs with the last 4 digits
-masked_account_number = 'XXXXXX' + last_four
+# "X" * (len(account_number) - 4) creates enough X characters to hide all but the last 4 digits
+# account_number[-4:] slices the string to get the last 4 characters of the account number
+# The two parts are joined together using + to form the masked account number
+masked_account_number = 'X' * (len(account_number) - 4) + account_number[-4:]
 
 # Print the masked account number
 print(f'The last 4 digits of your account are: {masked_account_number}')
