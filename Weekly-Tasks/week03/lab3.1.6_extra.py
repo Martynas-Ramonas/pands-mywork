574913adc4703ec9379a7f436f4e5a02c37f96dc
Martynas-Ramonas <G00486620@atu.ie>
2026-03-11 18:35:49 +0000

    Fixed the problem by using str() or just making 99 into a variable

--- Weekly-Tasks/week03/lab3.1.6_extra.py
+++ Weekly-Tasks/week03/lab3.1.6_extra.py
@@ -3,10 +3,20 @@
 # Author: Martynas Ramonas
 
 #Why does this expression cause an error? How can you fix it? 
-message = 'I have eaten ' + 99 + ' burritos.'
+#message = 'I have eaten ' + 99 + ' burritos.'
 
-
-
-
+# Turn hte number into a string using the str() function so it can be concatenated with the other strings.
+# Could also use an f-string to format the message instead of concatenating strings together.
+message = 'I have eaten ' + str(99) + ' burritos.'
+message2 = f'I have eaten {99} burritos.'
 
 print(message)
+print(message2)
+
+# Could also declate the number as a variable and use it in the message. But more work to do it this way. Unless you need to use the number in other places in the program.
+buritos_eaten = 99
+
+print(f'I have eaten {buritos_eaten} burritos.')
+print('I have eaten {} burritos.'.format(buritos_eaten))
+
+
