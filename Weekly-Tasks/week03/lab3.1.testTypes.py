3137d3b916f8c110309f1bbbc4808cb2a1d87acf
Martynas-Ramonas <G00486620@atu.ie>
2026-03-11 17:47:35 +0000

    Variables and types

---
+++ Weekly-Tasks/week03/lab3.1.testTypes.py
@@ -0,0 +1,25 @@
+# Weekly Tasks 3
+# Variables and States
+# Author: Martynas Ramonas
+
+# int
+i = 3
+
+# float
+fl = 3.5
+
+# bool
+isa = True
+
+# str
+memo = 'How now brown cow'
+
+# list
+lots = []
+
+# Print out the variables and their types
+print('variable {} is of type:{} and value:{}'.format('i', type(i), i))
+print('variable {} is of type:{} and value:{}'.format('fl', type(fl), fl))
+print('variable {} is of type:{} and value:{}'.format('is', type(isa), isa))
+print('variable {} is of type:{} and value:{}'.format('memo', type(memo), memo))
+print('variable {} is of type:{} and value:{}'.format('lots', type(lots), lots))
