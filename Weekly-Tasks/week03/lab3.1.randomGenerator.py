f41c873916ab445390e23525dcddcbf7c2fb433d
Martynas-Ramonas <G00486620@atu.ie>
2026-03-11 18:08:26 +0000

    Added user input for lowest/higest number for the random generator

--- Weekly-Tasks/week03/lab3.1.randomGenerator.py
+++ Weekly-Tasks/week03/lab3.1.randomGenerator.py
@@ -8,3 +8,14 @@ import random
 
 number = random.randint(1, 10)
 print('Here is a random number: {}'.format(number))
+
+# Modify the program so the user inputs the range of the random number.
+# Get the range from the user
+lowest_number = int(input('Enter the lowest number: '))
+highest_number = int(input('Enter the highest number: '))
+
+# randint() takes two arguments: the lowest number and the highest number. It returns a random integer between those two numbers (inclusive).
+random_number = random.randint(lowest_number, highest_number)
+
+# Prefer this formatting style over the one above. It is more readable and easier to write.
+print(f'Here is a random number from {lowest_number} to {highest_number}: {random_number}') 
