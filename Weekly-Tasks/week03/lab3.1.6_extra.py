426d172c6815a7429bfb02b5e248a60933e3e973
Martynas-Ramonas <G00486620@atu.ie>
2026-03-11 18:30:50 +0000

    Extra question initial set up

---
+++ Weekly-Tasks/week03/lab3.1.6_extra.py
@@ -0,0 +1,12 @@
+# Weekly Task 3 Extra
+# Fix the problem
+# Author: Martynas Ramonas
+
+#Why does this expression cause an error? How can you fix it? 
+message = 'I have eaten ' + 99 + ' burritos.'
+
+
+
+
+
+print(message)
