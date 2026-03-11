4a9c47167202a01d7e9dddb143584b7fad2b8be4
Martynas-Ramonas <G00486620@atu.ie>
2026-03-11 16:42:18 +0000

    Weekly task 02 the pank program that takes input in cents and outputs a total amount in euro and any decimals

---
+++ Weekly-Tasks/week02/bank.py
@@ -0,0 +1,15 @@
+# Weekly Tasks 2
+# Promt the user and read in two money amounts (in cents)
+# Add the two amounts, and store the total as an interger
+# Print out the answer in a human readable forma with a euro sign and decimal point between the euro and cent of the amount
+# Author: Martynas Ramonas
+
+# Get the user's input for the two amounts in cents
+amount1 = int(input('Please enter the first amount in cents: '))
+amount2 = int(input('Please enter the second amount in cents: '))
+
+# Add the two amounts and store the total as an integer
+total = amount1 + amount2
+
+# Print out the answer in a human readable format with a euro sign and decimal point between the euro and cent of the amount.
+print(f'The total amount is €{total/100:.2f}')      
