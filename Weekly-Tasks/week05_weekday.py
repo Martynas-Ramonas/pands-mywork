2da2f31c73a45a73b1c9cc97040618f3fe3e9098
Martynas <G00486620@atu.ie>
2026-04-27 18:34:09 +0100

    Week 5 Task done, and moved all tasks to a single folder and put weekXX_taskname.py  for naming

---
+++ Weekly-Tasks/week05_weekday.py
@@ -0,0 +1,22 @@
+# Program that outputs whether or not today is a weekday.
+# Found on https://www.w3schools.com/python/python_datetime.asp
+# Author: Martynas Ramonas
+
+# Import the datetime module to work with dates and times
+import datetime
+
+# Get today's date and time
+today = datetime.today()
+
+# The weekday() function is a built-in method of the datetime.date class in Python. It returns the day of the week as an integer, 
+# where Monday is 0 and Sunday is 6. This method is useful when you want to determine the day of the week for a given date.
+# https://www.geeksforgeeks.org/python/weekday-function-of-datetime-date-class-in-python/  I'm ading these links and commnets for 
+# myself because I'm probably going to forget how this works and I want to be able to find it easily in the future.
+day_number = today.weekday()
+
+# 0 = Monday to 4 = Friday are weekdays
+# 5 = Saturday and 6 = Sunday are not weekdays
+if day_number < 5:
+    print("Yes, today is a weekday.")
+else:
+    print("No, today is not a weekday.")
